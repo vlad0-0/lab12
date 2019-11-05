09939dd28cf040ba9d584dd6f9fef3b787d77b64
vlad0-0 <54848408+vlad0-0@users.noreply.github.com>
2019-11-05 13:34:25 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -3,5 +3,9 @@ def RingS(r1, r2):
     return(3.14*(r1+r2)*abs(r1-r2))
 for i in range(3):
     r1 = float(input())
+    while r1 < 0:
+        r1 = float(input())
     r2 = float(input())
+    while r2 < 0:
+        r2 = float(input())
     print(RingS(r1, r2))
