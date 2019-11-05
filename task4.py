d0afb8b8430fde6afc8adaf9e96964ec5e3a9363
vlad0-0 <54848408+vlad0-0@users.noreply.github.com>
2019-11-05 13:13:26 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,1 +1,14 @@
-
+def Quarter(x, y):
+    if x > 0 and y > 0:
+        return(1)
+    elif x < 0 and y > 0:
+        return(2)
+    elif x < 0 and y > 0:
+        return(3)
+    elif x > 0 and y < 0:
+        return(4)
+    else:
+        return(0)
+x = float(input())
+y = float(input())
+print(Quarter(x, y))
